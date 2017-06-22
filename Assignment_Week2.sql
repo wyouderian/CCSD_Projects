146771f8f20b3ef1c48e6e6c84c54cc558bd2f60
wyouderian <wyouderian@gmail.com>
2017-06-22 12:35:38 -0700

    Added all modifications to Week 2

---
+++ Assignment_Week2.sql
@@ -0,0 +1,22 @@
+
+
+#Week 2 - Assignment pg. 91
+#Question 1
+create table SALES_BRANCH
+(BRANCH_NUM int primary key,
+BRANCH_NAME char(50),
+BRANCH_LOCATION char(50),
+NUM_EMPLOYEES int
+);
+
+#Question 2
+insert into SALES_BRANCH values(5,'Henry Town Plaza', '165 Plaza', 3);
+
+#Next, we display our entry
+select * from SALES_BRANCH;
+
+#Question 3
+drop table SALES_BRANCH;
+
+#Question 4,5,6 - done last week 
+
---
+++ Assignment_Week2_pg133.sql
@@ -0,0 +1,13 @@
+#Question 1
+select book_code, type, title from book;
+
+#Question 2
+select * from branch;
+
+#Question 3
+select publisher_name from publisher where (city = 'Boston');
+
+#Question 4
+select publisher_name, city from publisher where (city <>'New York');
+
+#Question 5
